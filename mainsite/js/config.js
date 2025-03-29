df5ab92a7897da3d0619163263cb7b07fa0417ef
Shombhunath Karan <karanshombhu@gmail.com>
2025-03-29 18:10:18 +0530

    Delete mainsite directory

--- mainsite/js/config.js
+++
@@ -1,9 +0,0 @@
-// API Configuration
-const API_CONFIG = {
-    BASE_URL: 'https://busseva-backend.onrender.com/api',
-    ENDPOINTS: {
-        BUSES: '/buses',
-        ROUTES: '/routes',
-        STOPS: '/stops'
-    }
-}; 
